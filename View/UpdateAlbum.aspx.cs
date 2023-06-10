56c49502924e14f6e3771784fa652c146093c906
Jonathan Emmanuel <93699344+dumpling7@users.noreply.github.com>
2023-06-10 23:58:32 +0700

    Revert "created UpdateAlbum backend and updated AlbumRepository (to be fixed)"
    This reverts commit 30e947d6003d069ba019008d4c05fd58e664ad29.

--- View/UpdateAlbum.aspx.cs
+++ View/UpdateAlbum.aspx.cs
@@ -4,9 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using Kpop_Ztation.Controller;
-using Kpop_Ztation.Model;
-using Kpop_Ztation.Repository;
 
 namespace Kpop_Ztation.View
 {
@@ -14,73 +11,7 @@ namespace Kpop_Ztation.View
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (Session["User"] == null)
-            {
-                Response.Redirect("../View/LoginPage.aspx");
-            }
-        }
 
-        protected void UpdateAlbumButton_Click(object sender, EventArgs e)
-        {
-            int albumID = Convert.ToInt32(Request.QueryString["albumID"]);
-            string name = nameTxt.Text;
-            string desc = descTxt.Text;
-            int price = Convert.ToInt32(priceTxt.Text);
-            int stock = Convert.ToInt32(stockTxt.Text);
-
-            Album album = AlbumRepository.GetAlbumByAlbumID(albumID);
-            if (album != null)
-            {
-                if (!string.IsNullOrEmpty(name))
-                    album.AlbumName = name;
-
-                if (!string.IsNullOrEmpty(desc))
-                    album.AlbumDescription = desc;
-
-                if (price > 0)
-                    album.AlbumPrice = price;
-
-                if (stock >= 0)
-                    album.AlbumStock = stock;
-
-                if (ImageUpload.HasFile)
-                {
-                    string imageExtension = Path.GetExtension(ImageUpload.FileName);
-                    int fileSize = ImageUpload.PostedFile.ContentLength;
-
-                    if (imageExtension != ".png" && imageExtension != ".jpg" && imageExtension != ".jpeg" && imageExtension != ".jfif")
-                    {
-                        errorTxt.Text = "Incorrect image type. Uploaded image can only be .png, .jpg, .jpeg, or .jfif";
-                        return;
-                    }
-
-                    int maxFileSize = 2 * 1024 * 1024;
-                    if (fileSize > maxFileSize)
-                    {
-                        errorTxt.Text = "Image cannot be larger than 2MB";
-                        return;
-                    }
-
-                    string imagePath = SaveImage(ImageUpload);
-                    album.ImagePath = imagePath;
-                }
-
-                AlbumRepository.UpdateAlbum(album);
-                Response.Redirect("AlbumDetails.aspx?albumID=" + albumID);
-            }
-            else
-            {
-                errorTxt.Text = "Album not found";
-            }
-        }
-
-        private string SaveImage(FileUpload fileUpload)
-        {
-            string fileName = Path.GetFileName(fileUpload.FileName);
-            string uniqueFileName = Guid.NewGuid().ToString() + "_" + fileName;
-            string filePath = Server.MapPath("~/Images/" + uniqueFileName);
-            fileUpload.SaveAs(filePath);
-            return "~/Images/" + uniqueFileName;
         }
     }
 }
