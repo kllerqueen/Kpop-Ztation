66e92d8cad30b1f6c918f4f4d0e7578aace09121
Nicholas <nicholasvincentleandro@gmail.com>
2023-06-12 01:15:46 +0700

    Cart Page

--- Repository/CartRepository.cs
+++ Repository/CartRepository.cs
@@ -1,14 +1,19 @@
-﻿using Kpop_Ztation.Model;
+﻿using Kpop_Ztation.Controller;
+using Kpop_Ztation.Repository;
+using Kpop_Ztation.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 
 namespace Kpop_Ztation.Repository
 {
     public class CartRepository
     {
         static KpopZtationDatabaseEntities1 db = new KpopZtationDatabaseEntities1();
+
 
         public static void CreateCart(Cart newCart)
         {
@@ -22,5 +27,6 @@ namespace Kpop_Ztation.Repository
             db.SaveChanges();
             return;
         }
+
     }
 }
