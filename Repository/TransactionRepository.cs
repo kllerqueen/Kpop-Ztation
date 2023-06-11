1d777aaf9e10d8cbf6cc5c788faa702d3ca32825
Nicholas <nicholasvincentleandro@gmail.com>
2023-06-12 02:03:11 +0700

    Revert "Revert "Cart Page""
    This reverts commit e473254ecd4c53a26a0153e772cf99eb2995b32e.

---
+++ Repository/TransactionRepository.cs
@@ -0,0 +1,39 @@
+﻿using Kpop_Ztation.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Kpop_Ztation.Repository
+{
+    public class TransactionRepository
+    {
+        static KpopZtationDatabaseEntities1 db = new KpopZtationDatabaseEntities1();
+
+        public static void CreateTransactionHeader(TransactionHeader th)
+        {
+            db.TransactionHeaders.Add(th);
+            db.SaveChanges();
+            return;
+        }
+
+        public static void UpdateTransactionHeader(TransactionHeader th)
+        {
+            db.SaveChanges();
+            return;
+        }
+
+        public static void CreateTransactionDetail(TransactionDetail td)
+        {
+            db.TransactionDetails.Add(td);
+            db.SaveChanges();
+            return;
+        }
+
+        public static void UpdateTransactionDetail(TransactionDetail td)
+        {
+            db.SaveChanges();
+            return;
+        }
+    }
+}
